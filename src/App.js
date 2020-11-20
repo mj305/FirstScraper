e0799ce8549cbc01acb687c49217a3b861962461
mj305 <maria.j.beckles@gmail.com>
2020-11-19 20:42:37 -0500

    added react-admin tutorial official

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import * as React from 'react';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>HELLO FROM APP JS</h1>
-    </div>
-  );
-}
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const App = () => (
+  <Admin dataProvider={dataProvider} >
+    <Resource name='users' list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
